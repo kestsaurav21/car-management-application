fda5a13d68ec18da44afb1fd2ea6a1bb68c655ba
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-15 22:25:51 +0530

    car route

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const mongoose = require("mongoose");
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swaggerConfig');
 const cors = require('cors');
-
 require('dotenv').config()
 
 const app = express();
@@ -12,6 +12,13 @@ app.use(express.json());
 //Connection to MongoDB
 mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
 
+
+
+app.use('/api/home', require('./routes/welcome'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/service', require('./routes/car'));
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 const PORT = 5005
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,15 +1,14 @@
-require("dotenv").config();
 const express = require("express");
-const User = require("../models/User");
-const jwt = require("jsonwebtoken");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
+const { Login, Signup } = require("../controllers/auth");
 
 /**
  * @openapi
  * /auth/user/signup:
  *   post:
  *     summary: Create a new user
+ *     tags:
+ *       - User
  *     requestBody:
  *       required: true
  *       content:
@@ -44,50 +43,15 @@ const bcrypt = require("bcryptjs");
  *       500:
  *         description: Server error
  */
-router.post("/user/signup", async (req, res) => {
-  try {
-    const { fullname, username, email, password } = req.body;
-
-    const existingUserEmail = await User.findOne({ email });
-
-    if (existingUserEmail) {
-      return res
-        .status(409)
-        .json({ error: "User with this email already exists" });
-    }
-
-    const existingUserName = await User.findOne({ username });
-
-    if (existingUserName) {
-      return res.status(409).json({ error: "Username already exists" });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({
-      fullname,
-      username,
-      email,
-      password: hashedPassword,
-    });
-    await newUser.save();
-
-    // sign token - jwt
-    const token = jwt.sign({ username, email }, process.env.SECRET);
-
-    return res.status(200).json({
-      token,
-    });
-  } catch (err) {
-    console.log("ROUTE: /user/signup - ", err);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
+router.post("/user/signup", Signup);
 
 /**
  * @openapi
  * /auth/user/login:
  *   post:
  *     summary: Login user
+ *     tags:
+ *       - User
  *     requestBody:
  *       required: true
  *       content:
@@ -113,40 +77,6 @@ router.post("/user/signup", async (req, res) => {
  *       500:
  *         description: Server error
  */
-router.post("/user/login", async (req, res) => {
-  try {
-    const { userId, password } = req.body;
-
-    const existingUser = await User.findOne({
-      $or: [{ email: userId }, { username: userId }],
-    });
-
-    if (!existingUser) {
-      return res.status(408).json({
-        error: "User does not exist",
-      });
-    }
-
-    const isMatch = await bcrypt.compare(password, existingUser.password);
-
-    const { username, email } = existingUser;
-
-    if (isMatch) {
-      // sign token - jwt
-      const token = jwt.sign({ username, email }, process.env.SECRET);
-
-      return res.status(200).json({
-        token
-      });
-    } else {
-      return res.status(401).json({
-        error: "Invalid Password",
-      });
-    }
-  } catch(err) {
-    console.log("ROUTE: /user/login - ", err);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
+router.post("/user/login", Login);
 
 module.exports = router;
