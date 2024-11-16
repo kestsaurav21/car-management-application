2c6d0cf17f0dbfb5bfda7ae9df5804597beef448
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-16 15:36:10 +0530

    car list and search route

--- server/controllers/car.js
+++ server/controllers/car.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const Car = require("../models/Car");
 
 const addCar = async (req, res) => {
@@ -45,7 +46,92 @@ const getCars = async (req, res) => {
   }
 };
 
+const searchCar = async (req, res) => {
+  try {
+    // Ensure the keyword is treated as a string
+    const keyword = String(req.query.keyword || "");
+
+    // Validate that a keyword is provided and is not empty
+    if (!keyword || keyword === "[object Object]") {
+      return res
+        .status(400)
+        .json({
+          error: "Keyword is required for search and must be a valid string",
+        });
+    }
+
+    const cars = await Car.find({
+      $or: [
+        { company: { $regex: keyword, $options: "i" } },
+        { model: { $regex: keyword, $options: "i" } },
+        { description: { $regex: keyword, $options: "i" } },
+        { tags: { $elemMatch: { $regex: keyword, $options: "i" } } },
+      ],
+    });
+
+    res.status(200).json(cars);
+  } catch (error) {
+    console.log("ROUTE: /car/search - ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const updateCarDetails = async (req, res) => {
+  try {
+    const carId = req.params.id;
+
+    // Check if carId is a valid ObjectId
+    if (!mongoose.isValidObjectId(carId)) {
+      return res.status(400).json({ error: "Invalid car ID format" });
+    }
+
+    console.log(req.body);
+    
+    const updateData = req.body;
+
+    // Update the car details
+    const updatedCar = await Car.findByIdAndUpdate(carId, updateData, {
+      new: true,
+    });
+
+    if (!updatedCar) {
+      return res.status(404).json({ error: "Car not found" });
+    }
+
+    res.status(200).json({
+      message: "Car details updated successfully",
+      car: updatedCar,
+    });
+  } catch (error) {
+    console.log("ROUTE: /car/update - ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+
+const deleteCar = async (req, res) => {
+
+  try {
+
+    const carId = req.params.id;
+    const car = await Car.findByIdAndDelete(carId);
+
+    if (!car) {
+      return res.status(404).json({ message: 'Car not found' });
+  }
+  
+    res.status(200).json({ message: 'Car deleted successfully' });
+    
+  } catch (error) {
+    console.log("ROUTE: /car/delete - ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
 module.exports = {
   addCar,
   getCars,
+  searchCar,
+  updateCarDetails,
+  deleteCar
 };
