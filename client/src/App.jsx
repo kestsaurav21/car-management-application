24d38cc3a69bc2334c94e0c2c1eaf5d84813d62d
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-17 20:36:07 +0530

    toast notification

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Home } from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import ProductListing from './pages/ProductListing'
+import { Toaster } from "@/components/ui/toaster"
+import ProductDetails from './pages/ProductDetails'
 
 function App() {
 
   return (
     <div className="flex items-center justify-center">
-      <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/productList" element={<ProductListing />} />
+          <Route path="/car/:id" element={<ProductDetails />} />
+        </Routes>
+        <Toaster />
     </div>
   )
 }
